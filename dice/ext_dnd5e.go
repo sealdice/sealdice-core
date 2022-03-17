30b6bc30a46f08ba76bb277697a375a87e5f25a0
fy <fy0@qq.com>
2022-03-18 07:12:02 +0800

    加入coc7默认属性值

--- dice/ext_dnd5e.go
+++ dice/ext_dnd5e.go
@@ -5,7 +5,11 @@ func RegisterBuiltinExtDnd5e(self *Dice) {
 		Name:       "dnd5e", // 扩展的名称，需要用于开启和关闭指令中，写简短点
 		Version:    "1.0.0",
 		Brief:      "不要看了，还没开始。咕咕咕",
+		Author:     "木落",
 		AutoActive: false, // 是否自动开启
+		ConflictWith: []string{
+			"coc7",
+		},
 		OnCommandReceived: func(ctx *MsgContext, msg *Message, cmdArgs *CmdArgs) {
 			//p := getPlayerInfoBySender(session, msg)
 			//p.TempValueAlias = &ac.Alias;
