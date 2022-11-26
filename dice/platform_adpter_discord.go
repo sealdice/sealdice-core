80c0121bf8054a62c608ea9498df44090a5aa35d
MSI-NB <szzrain@gmail.com>
2022-11-26 13:44:14 -0500

    Discord Support

--- dice/platform_adpter_discord.go
+++ dice/platform_adpter_discord.go
@@ -3,16 +3,20 @@ package dice
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"strconv"
 )
 
 type PlatformAdapterDiscord struct {
+	DiceServing   bool
 	Session       *IMSession
 	Token         string        `yaml:"token" json:"token"`
 	EndPoint      *EndPointInfo `yaml:"-" json:"-"`
 	IntentSession *discordgo.Session
 }
 
-func (pa *PlatformAdapterDiscord) GetGroupInfoAsync(groupId string) {}
+func (pa *PlatformAdapterDiscord) GetGroupInfoAsync(groupId string) {
+	return
+}
 
 func (pa *PlatformAdapterDiscord) Serve() int {
 	dg, err := discordgo.New("Bot " + pa.Token)
@@ -25,7 +29,8 @@ func (pa *PlatformAdapterDiscord) Serve() int {
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
-		pa.Session.Execute(pa.EndPoint, toStdMessage(m), false)
+		pa.Session.Parent.Logger.Infof("收到消息")
+		pa.Session.Execute(pa.EndPoint, toStdMessage(m), true)
 	})
 
 	// In this example, we only care about receiving message events.
@@ -49,12 +54,32 @@ func (pa *PlatformAdapterDiscord) SendToPerson(ctx *MsgContext, uid string, text
 }
 
 func (pa *PlatformAdapterDiscord) SendToGroup(ctx *MsgContext, uid string, text string, flag string) {
-	pa.IntentSession.ChannelMessageSend(uid, text)
+	//pa.EndPoint.Session.Parent.Logger.Infof("发送消息")
+	_, err := pa.IntentSession.ChannelMessageSend(uid, text)
+	if err != nil {
+		return
+	}
 }
 
 func (pa *PlatformAdapterDiscord) QuitGroup(ctx *MsgContext, id string) {}
 
 func (pa *PlatformAdapterDiscord) SetGroupCardName(groupId string, userId string, name string) {}
+
+func FormatDiceIdDiscord(diceDiscord int64) string {
+	return fmt.Sprintf("Discord:%s", strconv.FormatInt(diceDiscord, 10))
+}
+
+func FormatDiceIdDiscordGroup(diceDiscord string) string {
+	return fmt.Sprintf("Discord-Channel:%s", diceDiscord)
+}
+
+func FormatDiceIdDiscordCh(userId string) string {
+	return fmt.Sprintf("Discord-CH:%s", userId)
+}
+
+func FormatDiceIdDiscordChGroup(GuildId, ChannelId string) string {
+	return fmt.Sprintf("Discord-CH-Channel:%s-%s", GuildId, ChannelId)
+}
 
 func toStdMessage(m *discordgo.MessageCreate) *Message {
 	msg := new(Message)
@@ -63,7 +88,10 @@ func toStdMessage(m *discordgo.MessageCreate) *Message {
 	msg.RawId = m.ID
 	msg.GroupId = m.ChannelID
 	msg.Platform = "Discord"
+	msg.MessageType = "group"
 	send := new(SenderBase)
+	//DEBUG:记得删掉下面这行
+	send.GroupRole = "admin"
 	send.UserId = m.Author.ID
 	send.Nickname = m.Author.Username
 	msg.Sender = *send
