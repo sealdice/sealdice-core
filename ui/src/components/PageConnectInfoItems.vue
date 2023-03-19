d1fe44921caae7caec14b530f58a51d762a5c29f
fy <fy0748@gmail.com>
2023-03-19 23:19:37 +0800

    chore: fix, version 124

--- ui/src/components/PageConnectInfoItems.vue
+++ ui/src/components/PageConnectInfoItems.vue
@@ -40,7 +40,7 @@
                 <el-input v-model="smsCode"></el-input>
               </el-form-item>
               <el-form-item label="">
-                <el-button type="primary" @click="submitSmsCode(i)">提交</el-button>
+                <el-button :disabled="smsCode == ''" type="primary" @click="submitSmsCode(i)">提交</el-button>
               </el-form-item>
             </el-form>
           </div>
@@ -351,7 +351,7 @@
                   <div>{{ curConn.adapter?.goCqHttpSmsNumberTip }}</div>
                 </el-form-item>
                 <el-form-item label="验证码">
-                  <el-input v-model="smsCode"></el-input>
+                  <el-input :disabled="smsCode == ''" v-model="smsCode"></el-input>
                 </el-form-item>
                 <el-form-item label="">
                   <el-button type="primary" @click="submitSmsCode(curConn)">提交</el-button>
