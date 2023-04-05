a3f81e7e69fbe591478c5969c9da62e2b47e6b56
fy <fy0748@gmail.com>
2023-04-06 01:38:00 +0800

    fix: dnd的st不应该可以用来修改 #195

--- dice/dice.go
+++ dice/dice.go
@@ -23,7 +23,7 @@ import (
 )
 
 var APPNAME = "SealDice"
-var VERSION = "1.2.4 v20230319"
+var VERSION = "1.2.5dev v20230405"
 
 // var VERSION_CODE = int64(1001000) // 991404
 var VERSION_CODE = int64(1002004) // 坏了，1.1的版本号标错了，标成了1.10.0
