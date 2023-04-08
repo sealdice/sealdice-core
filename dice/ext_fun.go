f306b07fe1d7ffe493666168b56b02eed830ee46
fy <fy0748@gmail.com>
2023-04-09 04:16:07 +0800

    feat: 可自定义ww骰池的参数默认值，输入.ww help查看详情 #208

--- dice/ext_fun.go
+++ dice/ext_fun.go
@@ -555,6 +555,11 @@ func RegisterBuiltinExtFun(self *Dice) {
 		ShortHelp: ".dx 3c4",
 		Help:      "双重十字规则骰点:\n.dx 3c4 // 推荐使用.r 3c4替代",
 		Solve: func(ctx *MsgContext, msg *Message, cmdArgs *CmdArgs) CmdExecuteResult {
+			switch cmdArgs.GetArgN(1) {
+			case "help":
+				return CmdExecuteResult{Matched: true, Solved: true, ShowHelp: true}
+			}
+
 			txt := readNumber(cmdArgs.CleanArgs, "c10")
 			if txt == "" {
 				txt = "1c10"
@@ -570,11 +575,79 @@ func RegisterBuiltinExtFun(self *Dice) {
 	cmdWW := CmdItemInfo{
 		Name:      "ww",
 		ShortHelp: ".ww 10a5\n.ww 10",
-		Help:      "WOD/无限规则骰点:\n.ww 10a5 // 推荐使用.r 10a5替代\n.ww 10",
+		Help:      "WOD/无限规则骰点:\n.ww 10a5 // 推荐使用.r 10a5替代\n.ww 10 // 骰10a10\n.ww set k6 // 修改成功线为6(当前群)\n.ww set a8k6m9 // 修改其他默认设定\n.ww set clr // 取消修改",
 		Solve: func(ctx *MsgContext, msg *Message, cmdArgs *CmdArgs) CmdExecuteResult {
-			txt := readNumber(cmdArgs.CleanArgs, "a10")
+			switch cmdArgs.GetArgN(1) {
+			case "help":
+				return CmdExecuteResult{Matched: true, Solved: true, ShowHelp: true}
+			case "set":
+				arg2 := cmdArgs.GetArgN(2)
+				if arg2 == "clr" || arg2 == "clear" {
+					ctx.Group.ValueMap.Del("wodThreshold")
+					ctx.Group.ValueMap.Del("wodPoints")
+					ctx.Group.ValueMap.Del("wodAdd")
+					ctx.Group.UpdatedAtTime = time.Now().Unix()
+					ReplyToSender(ctx, msg, "骰池设定已恢复默认")
+					return CmdExecuteResult{Matched: true, Solved: true}
+				}
+
+				var texts []string
+
+				reK := regexp.MustCompile(`[kK](\d+)`)
+				if m := reK.FindStringSubmatch(arg2); len(m) > 0 {
+					if v, err := strconv.ParseInt(m[1], 10, 64); err == nil {
+						if v >= 1 {
+							ctx.Group.ValueMap.Set("wodThreshold", &VMValue{TypeId: VMTypeInt64, Value: v})
+							ctx.Group.UpdatedAtTime = time.Now().Unix()
+							texts = append(texts, fmt.Sprintf("成功线k: 已修改为%d", v))
+						} else {
+							texts = append(texts, fmt.Sprintf("成功线k: 需要至少为1"))
+						}
+					}
+				}
+				reM := regexp.MustCompile(`[mM](\d+)`)
+				if m := reM.FindStringSubmatch(arg2); len(m) > 0 {
+					if v, err := strconv.ParseInt(m[1], 10, 64); err == nil {
+						if v >= 1 && v <= 2000 {
+							ctx.Group.ValueMap.Set("wodPoints", &VMValue{TypeId: VMTypeInt64, Value: v})
+							ctx.Group.UpdatedAtTime = time.Now().Unix()
+							texts = append(texts, fmt.Sprintf("骰子面数m: 已修改为%d", v))
+						} else {
+							texts = append(texts, fmt.Sprintf("骰子面数m: 需要在1-2000之间"))
+						}
+					}
+				}
+				reA := regexp.MustCompile(`[aA](\d+)`)
+				if m := reA.FindStringSubmatch(arg2); len(m) > 0 {
+					if v, err := strconv.ParseInt(m[1], 10, 64); err == nil {
+						if v >= 2 {
+							ctx.Group.ValueMap.Set("wodAdd", &VMValue{TypeId: VMTypeInt64, Value: v})
+							ctx.Group.UpdatedAtTime = time.Now().Unix()
+							texts = append(texts, fmt.Sprintf("加骰线a: 已修改为%d", v))
+						} else {
+							texts = append(texts, fmt.Sprintf("加骰线a: 需要至少为2"))
+						}
+					}
+				}
+
+				if len(texts) == 0 {
+					return CmdExecuteResult{Matched: true, Solved: true, ShowHelp: true}
+				} else {
+					ReplyToSender(ctx, msg, strings.Join(texts, "\n"))
+					return CmdExecuteResult{Matched: true, Solved: true}
+				}
+			}
+
+			addNum := int64(10)
+			if adding, exists := ctx.Group.ValueMap.Get("wodAdd"); exists {
+				if t, ok := adding.(*VMValue); ok {
+					addNum, _ = t.ReadInt64()
+				}
+			}
+
+			txt := readNumber(cmdArgs.CleanArgs, fmt.Sprintf("a%d", addNum))
 			if txt == "" {
-				txt = "10a10"
+				txt = fmt.Sprintf("10a%d", addNum)
 				cmdArgs.Args = []string{txt}
 			}
 			cmdArgs.CleanArgs = txt
