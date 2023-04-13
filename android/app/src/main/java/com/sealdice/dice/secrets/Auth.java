9dc73a520c548aa00ed0aa398c76cc8569f15589
MSI-NB <szzrain@gmail.com>
2023-04-13 11:58:47 -0400

    :rocket: Auth.java

---
+++ android/app/src/main/java/com/sealdice/dice/secrets/Auth.java
@@ -0,0 +1,7 @@
+package com.sealdice.dice.secrets;
+
+public class Auth {
+    public static String ACRA_URL = "YOUR REPORT URL";
+    public static String ACRA_BASIC_AUTH = "YOUR AUTH";
+    public static String ACRA_LOGIN_PASS = "YOUR PASS";
+}
