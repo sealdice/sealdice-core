69f2bb89d6c28051ee2378a0ca31d81458b948b3
JustAnotherID <just-another-id@outlook.com>
2023-08-11 09:23:50 +0800

    feat(gocqhttp): apad的内置gocqhttp也支持填写签名服务 (#22)

--- ui/src/components/PageConnectInfoItems.vue
+++ ui/src/components/PageConnectInfoItems.vue
@@ -168,16 +168,16 @@
         </el-select>
       </el-form-item>
 
-      <el-form-item v-if="form.accountType === 0 && form.protocol === 1" label="使用签名服务" :label-width="formLabelWidth">
+      <el-form-item v-if="form.accountType === 0 && (form.protocol === 1 || form.protocol === 6)" label="使用签名服务" :label-width="formLabelWidth">
         <el-switch v-model="form.useSignServer" style="--el-switch-on-color: #67C23A;"/>
         <el-tooltip content="如果不知道这是什么请勿勾选。允许填写签名服务相关信息。" style="">
           <el-icon><QuestionFilled/></el-icon>
         </el-tooltip>
       </el-form-item>
-      <el-form-item v-if="form.accountType === 0 && form.protocol === 1 && form.useSignServer" label="签名服务url" :label-width="formLabelWidth">
+      <el-form-item v-if="form.accountType === 0 && (form.protocol === 1 || form.protocol === 6) && form.useSignServer" label="签名服务url" :label-width="formLabelWidth">
         <el-input v-model="form.signServerUrl" type="string" autocomplete="off" placeholder="http://127.0.0.1:8080"></el-input>
       </el-form-item>
-      <el-form-item v-if="form.accountType === 0 && form.protocol === 1 && form.useSignServer" label="签名服务key" :label-width="formLabelWidth">
+      <el-form-item v-if="form.accountType === 0 && (form.protocol === 1 || form.protocol === 6) && form.useSignServer" label="签名服务key" :label-width="formLabelWidth">
         <el-input v-model="form.signServerKey" type="string" autocomplete="off" placeholder="114514"></el-input>
       </el-form-item>
       <small>
@@ -243,16 +243,16 @@
             <!-- <div v-if="form.protocol !== 1" style="color: #aa4422;">提示: 首次登录时，iPad或者Android手表协议一般都会失败，建议用安卓登录后改协议。</div> -->
           </small>
         </el-form-item>
-        <el-form-item v-if="form.accountType === 0 && form.protocol === 1" label="使用签名服务" :label-width="formLabelWidth">
+        <el-form-item v-if="form.accountType === 0 && (form.protocol === 1 || form.protocol === 6)" label="使用签名服务" :label-width="formLabelWidth">
           <el-switch v-model="form.useSignServer" style="--el-switch-on-color: #67C23A;"/>
           <el-tooltip content="如果不知道这是什么请勿勾选。允许填写签名服务相关信息。" style="">
             <el-icon><QuestionFilled/></el-icon>
           </el-tooltip>
         </el-form-item>
-        <el-form-item v-if="form.accountType === 0 && form.protocol === 1 && form.useSignServer" label="签名服务url" :label-width="formLabelWidth">
+        <el-form-item v-if="form.accountType === 0 && (form.protocol === 1 || form.protocol === 6) && form.useSignServer" label="签名服务url" :label-width="formLabelWidth">
           <el-input v-model="form.signServerUrl" type="string" autocomplete="off" placeholder="http://127.0.0.1:8080"></el-input>
         </el-form-item>
-        <el-form-item v-if="form.accountType === 0 && form.protocol === 1 && form.useSignServer" label="签名服务key" :label-width="formLabelWidth">
+        <el-form-item v-if="form.accountType === 0 && (form.protocol === 1 || form.protocol === 6) && form.useSignServer" label="签名服务key" :label-width="formLabelWidth">
           <el-input v-model="form.signServerKey" type="string" autocomplete="off" placeholder="114514"></el-input>
         </el-form-item>
 
