65b7a178ac3a2726d1cc4fef2a7229878e84e994
檀轶步棋 <57583509+Verplitic@users.noreply.github.com>
2023-08-20 18:17:58 +0800

    fix: PR#140 导致无法发送通知的问题；添加 `OnGuildJoined` 事件并适配 KOOK 与 Discord (#153)
    * 添加 `OnGuildJoined` 事件并适配 KOOK 与 Discord
    
    * 修复一个导致不会发送通知的问题

--- dice/platform_adapter_discord.go
+++ dice/platform_adapter_discord.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	"net/url"
+	"runtime/debug"
 	"strconv"
 	"strings"
 	"time"
@@ -118,6 +119,67 @@ func (pa *PlatformAdapterDiscord) Serve() int {
 		msg.Time = m.Timestamp.Unix()
 		mctx := &MsgContext{Session: pa.Session, EndPoint: pa.EndPoint, Dice: pa.Session.Parent, MessageType: msg.MessageType}
 		pa.Session.OnMessageDeleted(mctx, msg)
+	})
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.GuildCreate) {
+		msg := new(Message)
+		msg.Time = m.JoinedAt.Unix()
+		msg.Platform = "DISCORD"
+
+		msg.GuildId = FormatDiceIdDiscordGuild(m.ID)
+		// 一些服务器的 SystemChannel 和 RulesChannel 可能都为空
+		if m.SystemChannelID != "" {
+			msg.GroupId = FormatDiceIdDiscordChannel(m.SystemChannelID)
+		} else if m.RulesChannelID != "" {
+			msg.GroupId = FormatDiceIdDiscordChannel(m.RulesChannelID)
+		}
+
+		// 如果获取不到默认频道的话，入群致辞和 OnGuildJoined 基本上没什么意义
+		if msg.GroupId == "" {
+			return
+		}
+
+		msg.Sender.Nickname = "系统"
+		// GuildCreate 似乎不会在私聊消息时触发
+		msg.MessageType = "group"
+
+		mctx := &MsgContext{Session: pa.Session, EndPoint: pa.EndPoint, Dice: pa.Session.Parent, MessageType: msg.MessageType}
+		pa.GetGroupInfoAsync(msg.GroupId)
+		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					pa.Session.Parent.Logger.Errorf("入群致辞异常: %v 堆栈: %v", r, string(debug.Stack()))
+				}
+			}()
+
+			// 稍作等待后发送入群致词
+			time.Sleep(1 * time.Second)
+
+			mctx.Player = &GroupPlayerInfo{}
+			pa.Session.Parent.Logger.Infof("发送入群致辞，群: <%s>(%s)", m.Name, msg.GuildId)
+			text := DiceFormatTmpl(mctx, "核心:骰子进群")
+			for _, i := range strings.Split(text, "###SPLIT###") {
+				pa.SendToGroup(mctx, msg.GroupId, strings.TrimSpace(i), "")
+			}
+		}()
+
+		// 此时 ServiceAtNew 中这个频道一般为空，照 im_session.go 中的方法处理
+		channel := mctx.Session.ServiceAtNew[msg.GroupId]
+		if channel == nil {
+			channel = SetBotOnAtGroup(mctx, msg.GroupId)
+			channel.Active = true
+			channel.DiceIdExistsMap.Store(pa.EndPoint.UserId, true)
+			channel.UpdatedAtTime = time.Now().Unix()
+		}
+
+		if mctx.Session.ServiceAtNew[msg.GroupId] != nil {
+			for _, i := range mctx.Session.ServiceAtNew[msg.GroupId].ActivatedExtList {
+				if i.OnGuildJoined != nil {
+					i.callWithJsCheck(mctx.Dice, func() {
+						i.OnGuildJoined(mctx, msg)
+					})
+				}
+			}
+		}
 	})
 	//这里只处理消息，未来根据需要再改这里
 	dg.Identify.Intents = discordgo.IntentsAll
