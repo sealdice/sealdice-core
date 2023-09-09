469d02b5c939728c49897de881e1e53fe2139140
fy <fy0748@gmail.com>
2023-09-09 13:36:08 +0000

    feat: add update-submodules.sh

---
+++ build/update-submodules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git submodule update
+cd sealdice-builtins && git pull origin master && cd ..
+cd sealdice-ui && git pull origin master && cd ..
+cd sealdice-android && git pull origin master && cd ..
+cd go-cqhttp && git pull origin master && cd ..
+cd sealdice-core && git pull origin master && cd ..
