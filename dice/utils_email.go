16abdb03087cfa8e59573c3b19496c56243f213a
JustAnotherID <just-another-id@outlook.com>
2023-09-18 15:27:11 +0800

    fix(email): email内容为空时，后缀不换行

--- dice/utils_email.go
+++ dice/utils_email.go
@@ -59,7 +59,11 @@ func (d *Dice) SendMailRow(subject string, to []string, content string, attachme
 	m.SetHeader("Subject", subject)
 	m.SetHeader("From", d.MailFrom)
 	m.SetHeader("To", to...)
-	m.SetBody("text/plain", content+"\n\n自动邮件，无需回复。")
+	if content == "" {
+		m.SetBody("text/plain", "自动邮件，无需回复。")
+	} else {
+		m.SetBody("text/plain", content+"\n\n自动邮件，无需回复。")
+	}
 	if len(attachments) > 0 {
 		for _, attachment := range attachments {
 			m.Attach(attachment)
