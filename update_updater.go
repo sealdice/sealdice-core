6cb8c4b7ea87e4dc92f7fec1a87f446811cb3cd3
fy <fy0748@gmail.com>
2023-09-19 20:19:34 +0800

    imp: 尝试适配升级程序到macos和linux

--- update_updater.go
+++ update_updater.go
@@ -9,7 +9,6 @@ import (
 	"sealdice-core/dice"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 )
 
@@ -100,21 +99,23 @@ func UpdateByFile(dm *dice.DiceManager, packName string) bool {
 		if ctx.Err() == context.DeadlineExceeded {
 			logger.Info("升级程序: 验证成功，进入下一阶段，即将退出进程")
 
-			if runtime.GOOS == "windows" {
-				logger.Info("升级程序: 参数 ", args)
-				cmd := executeWin(fn, args...)
-				err := cmd.Start()
-				if err != nil {
-					logger.Error("升级程序: 执行失败 ", err.Error())
-					return false
-				}
-			} else {
-				err := syscall.Exec(fn, args, os.Environ())
-				if err != nil {
-					logger.Error("升级程序: 执行失败 ", err.Error())
-					return false
-				}
+			//if runtime.GOOS == "windows" {
+			logger.Info("升级程序: 参数 ", args)
+			cmd := executeWin(fn, args...)
+			err := cmd.Start()
+			if err != nil {
+				logger.Error("升级程序: 执行失败 ", err.Error())
+				return false
 			}
+			//} else {
+			//	args = append([]string{fn}, args...)
+			//	logger.Info("升级程序: 参数 ", args)
+			//	err := syscall.Exec(fn, args, os.Environ())
+			//	if err != nil {
+			//		logger.Error("升级程序: 执行失败 ", err.Error())
+			//		return false
+			//	}
+			//}
 
 			time.Sleep(5 * time.Second)
 			cleanUpCreate(dm)()
