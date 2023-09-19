f90db59f00d45a911d7d7612f51fdfe24cc9f5b4
Xiangze Li <lee_johnson@qq.com>
2023-09-19 21:48:59 +0800

    feat(Log): Log未更新时不重复向服务器上传 (#236)
    上传时先检查DB中记录的URL和上传时间, 如果有URL且上传时间晚于更新时间, 直接返回存储的URL
    否则, 执行上传, 得到的URL落库
    
    Close #61

--- api/story_log.go
+++ api/story_log.go
@@ -1,18 +1,10 @@
 package api
 
 import (
-	"archive/zip"
-	"bytes"
-	"compress/zlib"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
 	"sealdice-core/dice"
 	"sealdice-core/dice/model"
-	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -138,61 +130,10 @@ func storyUploadLog(c echo.Context) error {
 }
 
 func logSendToBackend(groupId string, logName string) (string, error) {
-	dirpath := filepath.Join(myDice.BaseConfig.DataDir, "log-exports")
-	_ = os.MkdirAll(dirpath, 0755)
-
-	lines, err := model.LogGetAllLines(myDice.DBLogs, groupId, logName)
-
-	if len(lines) == 0 {
-		return "", errors.New("#此log不存在，或条目数为空，名字是否正确？")
+	ctx := &dice.MsgContext{
+		Dice:     myDice,
+		EndPoint: myDice.UIEndpoint,
 	}
 
-	if err != nil {
-		return "", err
-	}
-
-	if err == nil {
-		// 本地进行一个zip留档，以防万一
-		fzip, _ := os.CreateTemp(dirpath, dice.FilenameReplace(groupId+"_"+logName)+".*.zip")
-		writer := zip.NewWriter(fzip)
-
-		text := ""
-		for _, i := range lines {
-			timeTxt := time.Unix(i.Time, 0).Format("2006-01-02 15:04:05")
-			text += fmt.Sprintf("%s(%v) %s\n%s\n\n", i.Nickname, i.IMUserId, timeTxt, i.Message)
-		}
-
-		fileWriter, _ := writer.Create("文本log.txt")
-		_, _ = fileWriter.Write([]byte(text))
-
-		data, err := json.Marshal(map[string]interface{}{
-			"version": dice.Story_version,
-			"items":   lines,
-		})
-		if err == nil {
-			fileWriter2, _ := writer.Create("海豹标准log-粘贴到染色器可格式化.txt")
-			_, _ = fileWriter2.Write(data)
-		}
-
-		_ = writer.Close()
-		_ = fzip.Close()
-	}
-
-	if err == nil {
-		// 压缩log，发往后端
-		data, err := json.Marshal(map[string]interface{}{
-			"version": dice.Story_version,
-			"items":   lines,
-		})
-
-		if err == nil {
-			var zlibBuffer bytes.Buffer
-			w := zlib.NewWriter(&zlibBuffer)
-			_, _ = w.Write(data)
-			_ = w.Close()
-
-			return dice.UploadFileToWeizaima(myDice.Logger, logName, myDice.UIEndpoint.UserId, &zlibBuffer), nil
-		}
-	}
-	return "", nil
+	return dice.LogSendToBackend(ctx, groupId, logName)
 }
