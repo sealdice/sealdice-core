b80dbc54bb5705a96cc78e980d70e624f2871b98
fy <fy0748@gmail.com>
2023-09-25 20:55:46 +0800

    fix: 修复.master新机制引起的私聊不回复

--- dice/builtin_commands.go
+++ dice/builtin_commands.go
@@ -698,7 +698,8 @@ func (d *Dice) registerCoreCommands() {
 
 			if !(subCmd == "add" || subCmd == "del" || subCmd == "rm") {
 				// 如果不是add/del/rm，那么就不需要代骰
-				if !cmdArgs.AmIBeMentionedFirst {
+				// 补充，在组内才这样，私聊不需要at
+				if ctx.MessageType == "Group" && !cmdArgs.AmIBeMentionedFirst {
 					return CmdExecuteResult{Matched: false, Solved: false}
 				}
 			}
