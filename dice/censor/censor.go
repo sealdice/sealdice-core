ac744e68005374b8c79030a7906aeffd1ec1fe23
JustAnotherID <just-another-id@outlook.com>
2023-09-26 11:54:02 +0800

    feat(censor): 敏感词库支持允许填写更多信息的toml格式

--- dice/censor/censor.go
+++ dice/censor/censor.go
@@ -2,10 +2,13 @@ package censor
 
 import (
 	"bufio"
+	"github.com/BurntSushi/toml"
 	"io"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
 	nanoid "github.com/matoous/go-nanoid/v2"
 
@@ -74,11 +77,28 @@ type WordFile struct {
 	Key         string
 	Path        string
 	FileCounter *FileCounter
+
+	FileType   string
+	Name       string
+	Authors    []string
+	Version    string
+	Desc       string
+	License    string
+	Date       time.Time
+	UpdateDate time.Time
 }
 
 type FileCounter [5]int
 
 func (c *Censor) PreloadFile(path string) (*WordFile, error) {
+	if strings.ToLower(filepath.Ext(path)) == ".toml" {
+		return c.tryPreloadTomlFile(path)
+	} else {
+		return c.tryPreloadTxtFile(path)
+	}
+}
+
+func (c *Censor) tryPreloadTxtFile(path string) (*WordFile, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -88,7 +108,6 @@ func (c *Censor) PreloadFile(path string) (*WordFile, error) {
 	}(file)
 
 	curLevel := Ignore
-	c.SensitiveKeys = make(map[string]WordInfo)
 	reader := bufio.NewReader(file)
 	var counter FileCounter
 	for {
@@ -110,28 +129,7 @@ func (c *Censor) PreloadFile(path string) (*WordFile, error) {
 					curLevel = Danger
 				}
 			} else {
-				key := strings.ToLower(strings.TrimSpace(word))
-				counter[curLevel]++
-				if c.CaseSensitive {
-					c.SensitiveKeys[key] = WordInfo{Level: curLevel}
-				} else {
-					if c.MatchPinyin {
-						// 拼音必须大小写不敏感
-						w := strings.ToLower(key)
-						c.SensitiveKeys[w] = WordInfo{Level: curLevel, Origin: key, Reason: IgnoreCase}
-
-						pys := pinyin.LazyPinyin(w, pinyin.Args{
-							Style: pinyin.Normal,
-							Fallback: func(r rune, a pinyin.Args) []string {
-								return []string{string(r)}
-							},
-						})
-						pyStr := strings.Join(pys, "")
-						c.SensitiveKeys[strings.ToLower(pyStr)] = WordInfo{Level: curLevel, Origin: key, Reason: PinYin}
-					} else {
-						c.SensitiveKeys[strings.ToLower(key)] = WordInfo{Level: curLevel, Origin: key, Reason: IgnoreCase}
-					}
-				}
+				c.addWord(word, curLevel, &counter)
 			}
 		}
 
@@ -147,7 +145,100 @@ func (c *Censor) PreloadFile(path string) (*WordFile, error) {
 		Key:         generateFileKey(),
 		Path:        path,
 		FileCounter: &counter,
+		FileType:    "txt",
+		Name:        filepath.Base(path),
 	}, nil
+}
+
+type TomlCensorWordFile struct {
+	Meta struct {
+		Name       string    `toml:"name"`
+		Author     string    `toml:"author"`
+		Authors    []string  `toml:"authors"`
+		Version    string    `toml:"version"`
+		Desc       string    `toml:"desc"`
+		License    string    `toml:"license"`
+		Date       time.Time `toml:"date"`
+		UpdateDate time.Time `toml:"updateDate"`
+	} `toml:"meta"`
+	Words struct {
+		Ignore  []string `toml:"ignore"`
+		Notice  []string `toml:"notice"`
+		Caution []string `toml:"caution"`
+		Warning []string `toml:"warning"`
+		Danger  []string `toml:"danger"`
+	} `toml:"words"`
+}
+
+func (c *Censor) tryPreloadTomlFile(path string) (*WordFile, error) {
+	var tomlFile *TomlCensorWordFile
+	if _, err := toml.DecodeFile(path, &tomlFile); err != nil {
+		return nil, err
+	}
+
+	var counter FileCounter
+	for _, word := range tomlFile.Words.Ignore {
+		c.addWord(word, Ignore, &counter)
+	}
+	for _, word := range tomlFile.Words.Notice {
+		c.addWord(word, Notice, &counter)
+	}
+	for _, word := range tomlFile.Words.Caution {
+		c.addWord(word, Caution, &counter)
+	}
+	for _, word := range tomlFile.Words.Warning {
+		c.addWord(word, Warning, &counter)
+	}
+	for _, word := range tomlFile.Words.Danger {
+		c.addWord(word, Danger, &counter)
+	}
+
+	meta := tomlFile.Meta
+	if meta.Name == "" {
+		meta.Name = filepath.Base(path)
+	}
+	if meta.Author != "" && len(meta.Authors) == 0 {
+		meta.Authors = append(meta.Authors, meta.Author)
+	}
+
+	return &WordFile{
+		Key:         generateFileKey(),
+		Path:        path,
+		FileCounter: &counter,
+		FileType:    "toml",
+		Name:        meta.Name,
+		Authors:     meta.Authors,
+		Version:     meta.Version,
+		Desc:        meta.Desc,
+		License:     meta.License,
+		Date:        meta.Date,
+		UpdateDate:  meta.UpdateDate,
+	}, nil
+}
+
+func (c *Censor) addWord(word string, level Level, counter *FileCounter) {
+	key := strings.ToLower(strings.TrimSpace(word))
+	counter[level]++
+	if c.CaseSensitive {
+		c.SensitiveKeys[key] = WordInfo{Level: level}
+	} else {
+		if c.MatchPinyin {
+			// 拼音必须大小写不敏感
+			w := strings.ToLower(key)
+			c.SensitiveKeys[w] = WordInfo{Level: level, Origin: key, Reason: IgnoreCase}
+
+			pys := pinyin.LazyPinyin(w, pinyin.Args{
+				Style: pinyin.Normal,
+				Fallback: func(r rune, a pinyin.Args) []string {
+					return []string{string(r)}
+				},
+			})
+			pyStr := strings.Join(pys, "")
+			c.SensitiveKeys[strings.ToLower(pyStr)] = WordInfo{Level: level, Origin: key, Reason: PinYin}
+		} else {
+			c.SensitiveKeys[strings.ToLower(key)] = WordInfo{Level: level, Origin: key, Reason: IgnoreCase}
+		}
+	}
 }
 
 func (c *Censor) Load() (err error) {
