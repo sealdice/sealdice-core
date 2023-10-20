02502fbfdd6914200653f417c2e3108fe0da5f4d
JustAnotherID <just-another-id@outlook.com>
2023-10-19 22:26:11 -0500

    feat: 不清理从未发言过的群，防止新加群被立即清理 (#357)

--- core/tray_darwin.go
+++ core/tray_darwin.go
@@ -124,7 +124,7 @@ func httpServe(e *echo.Echo, dm *dice.DiceManager, hideUI bool) {
 	}
 	_ = ln.Close()
 
-	//exec.Command(`cmd`, `/c`, `start`, fmt.Sprintf(`http://localhost:%s`, portStr)).Start()
+	// exec.Command(`cmd`, `/c`, `start`, fmt.Sprintf(`http://localhost:%s`, portStr)).Start()
 	fmt.Println("如果浏览器没有自动打开，请手动访问:")
 	fmt.Printf(`http://localhost:%s`, portStr) // 默认:3211
 	err = e.Start(dm.ServeAddress)
