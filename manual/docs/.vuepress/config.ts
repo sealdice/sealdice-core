3a948dfaeeba07563e8b448634e44807b3c42996
fy <fy0748@gmail.com>
2023-10-29 15:26:06 +0000

    ci: 增加环境变量决定BASE_PATH的功能

--- manual/docs/.vuepress/config.ts
+++ manual/docs/.vuepress/config.ts
@@ -2,8 +2,11 @@ import { defineUserConfig } from "vuepress";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme";
 
+console.log('xxxxxxxxx', process.env.BASE_PATH)
+const basePath = process.env.BASE_PATH ?? "/sealdice-manual-next/";
+
 export default defineUserConfig({
-  base: "/sealdice-manual-next/",
+  base: basePath,
   lang: "zh-CN",
   title: "海豹手册",
   description: "海豹核心的全新官方使用手册",
