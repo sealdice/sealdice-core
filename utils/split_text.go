273defce24b01bbee46938af2606ca7479af1f9f
Xiangze Li <lee_johnson@qq.com>
2023-11-16 14:52:44 +0800

    fix: 调整切分对空白的处理逻辑

--- utils/split_text.go
+++ utils/split_text.go
@@ -1,14 +1,12 @@
 package utils
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
 
 func splitFirst(s string, maxLen int) (first string, rest string) {
-	// 去除首尾空白
-	s = strings.TrimSpace(s)
-
 	// 不足上限不切分
 	if len(s) <= maxLen {
 		return s, ""
@@ -21,9 +19,10 @@ func splitFirst(s string, maxLen int) (first string, rest string) {
 	}
 
 	// 如果有连续换行符, 直接切分
-	idxTwoNL := strings.Index(s[0:r], "\n\n")
-	if idxTwoNL >= 0 {
-		return s[0:idxTwoNL], s[idxTwoNL+2:]
+	multiNL := regexp.MustCompile(`\n{2,}`)
+	idxMultiNL := multiNL.FindStringIndex(s[0:r])
+	if len(idxMultiNL) == 2 {
+		return s[0:idxMultiNL[0]], s[idxMultiNL[1]:]
 	}
 
 	// 如果切分中有换行符, 以最后一个换行符切分, 增强可读性
@@ -42,7 +41,7 @@ func splitFirst(s string, maxLen int) (first string, rest string) {
 // 确保切分后每个子串长度不大于 maxLen.
 // 优先以 text 中的换行符为切分点, 总长度小于 maxLen 的连续短行不切分.
 //
-// 例外: text 中的一个完全空行 (2 个连续换行符) 强制切分.
+// 例外: text 中的多个连续换行符会被强制切分.
 func SplitLongText(text string, maxLen int) []string {
 	if maxLen <= 0 {
 		return []string{text}
@@ -52,7 +51,9 @@ func SplitLongText(text string, maxLen int) []string {
 
 	for len(text) > 0 {
 		first, rest := splitFirst(text, maxLen)
-		splits = append(splits, first)
+		if len(strings.TrimSpace(first)) > 0 {
+			splits = append(splits, first)
+		}
 		text = rest
 	}
 
