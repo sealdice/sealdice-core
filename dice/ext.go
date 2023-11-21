40de93c102b744e8a46fd890a42325238f6b9098
Szzrain <54656633+Szzrain@users.noreply.github.com>
2023-11-20 22:25:31 -0500

    fix(JSVM): 修复修改原型链导致的一些问题 (#429)

--- dice/ext.go
+++ dice/ext.go
@@ -122,7 +122,7 @@ func (i *ExtInfo) StorageInit() error {
 	return err
 }
 
-func (i *ExtInfo) StorageSetRaw(k, v string) error {
+func (i *ExtInfo) StorageSet(k, v string) error {
 	if err := i.StorageInit(); err != nil {
 		return err
 	}
@@ -134,7 +134,7 @@ func (i *ExtInfo) StorageSetRaw(k, v string) error {
 	})
 }
 
-func (i *ExtInfo) StorageGetRaw(k string) (string, error) {
+func (i *ExtInfo) StorageGet(k string) (string, error) {
 	if err := i.StorageInit(); err != nil {
 		return "", err
 	}
@@ -144,7 +144,7 @@ func (i *ExtInfo) StorageGetRaw(k string) (string, error) {
 	db := i.Storage
 	err = db.View(func(tx *buntdb.Tx) error {
 		val, err = tx.Get(k)
-		if err != nil {
+		if err != nil && !errors.Is(err, buntdb.ErrNotFound) {
 			return err
 		}
 		return nil
