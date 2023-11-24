ecee63eaa65e58ecbe1f02bb1015e2f1a638d2d0
fy <fy0748@gmail.com>
2023-11-24 18:27:11 +0800

    fix: 修改shrink顺序

--- dice/model/backup.go
+++ dice/model/backup.go
@@ -10,7 +10,7 @@ func Vacuum(db *sqlx.DB, path string) error {
 }
 
 func FlushWAL(db *sqlx.DB) error {
-	_, _ = db.Exec("PRAGMA shrink_memory")
 	_, err := db.Exec("PRAGMA wal_checkpoint(TRUNCATE);")
+	_, _ = db.Exec("PRAGMA shrink_memory")
 	return err
 }
