98c9f278d275aa6609afb67a358d9eb2bba63997
Xiangze Li <lee_johnson@qq.com>
2024-01-02 20:37:53 +0800

    fix(log): 日志导出zip内部使用纯ASCII文件名,解决编码问题 (#515)
    Close #513

---
+++ dice/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+// 日志导出时使用的常量
+const (
+	LogExportTxtFilename    = "raw-log.txt"
+	LogExportJsonFilename   = "sealdice-standard-log.json"
+	LogExportReadmeFilename = "README.txt"
+	LogExportReadmeContent  = LogExportTxtFilename + ": 纯文本 Log\n" + LogExportJsonFilename + ": 海豹标准 Log, 粘贴到染色器可格式化\n"
+)
