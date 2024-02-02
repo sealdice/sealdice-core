1af06a4cc61405f65420acbb3b4ed441acba1ba4
火纸 <109738656+FlameTEXT@users.noreply.github.com>
2024-02-02 15:48:47 +0800

    fix(OfficialQQ): 不能“只在频道中使用” (#560)
    * fix(OfficialQQ): 不能“只在频道中使用”
    
    close: https://github.com/sealdice/sealdice-core/issues/530
    
    * golangci-lint run --fix
    
    ---------
    
    Co-authored-by: FlameTEXT <andor1417@qq.com>

--- dice/model/db_init.go
+++ dice/model/db_init.go
@@ -14,10 +14,10 @@ func _SQLiteDBInit(path string, useWAL bool) (*sqlx.DB, error) {
 		panic(err)
 	}
 
-	//_, err = db.Exec("vacuum")
-	//if err != nil {
-	//	panic(err)
-	//}
+	// _, err = db.Exec("vacuum")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// enable WAL mode
 	if useWAL {
