8c7f36bc2b477824dfbcbb9e9d721997eddf27e6
Szzrain <54656633+Szzrain@users.noreply.github.com>
2024-02-28 01:57:21 -0500

    fix: 修复 log off状态下进行log end，提示的文案错误的问题 (#606)

--- dice/ext_log.go
+++ dice/ext_log.go
@@ -13,7 +13,6 @@ import (
 	"runtime"
 	"strings"
 	"time"
-	"unicode"
 
 	"github.com/fy0/lockfree"
 	"github.com/golang-module/carbon"
@@ -297,9 +296,10 @@ func RegisterBuiltinExtLog(self *Dice) {
 
 				VarSetValueStr(ctx, "$t记录名称", group.LogCurName)
 				text := DiceFormatTmpl(ctx, "日志:记录_结束")
-				if !group.LogOn {
-					text = strings.TrimRightFunc(DiceFormatTmpl(ctx, "日志:记录_关闭_失败"), unicode.IsSpace) + "\n" + text
-				}
+				// Note: 2024-02-28 经过讨论，日志在 off 的情况下 end 属于合理操作，这里不再检查是否开启
+				// if !group.LogOn {
+				//	 text = strings.TrimRightFunc(DiceFormatTmpl(ctx, "日志:记录_关闭_失败"), unicode.IsSpace) + "\n" + text
+				// }
 				ReplyToSender(ctx, msg, text)
 				group.LogOn = false
 				group.UpdatedAtTime = time.Now().Unix()
