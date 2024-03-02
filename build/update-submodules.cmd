2bfb27d0db0e992707fa86ef4749f25d9e6641c7
JustAnotherID <just-another-id@outlook.com>
2024-03-02 21:07:37 +0800

    chore: bump submodules

--- build/update-submodules.cmd
+++ build/update-submodules.cmd
@@ -2,6 +2,6 @@
 git pull
 git submodule init
 git submodule update
-git submodule foreach git pull origin master
+git submodule foreach "git fetch origin --prune & git reset --hard origin/master"
 git commit -am "chore: bump submodules"
 git push
