ad94025fc857a75cb3352a6a26eec1c6f0f25314
JustAnotherID <just-another-id@outlook.com>
2024-03-09 14:23:46 +0800

    refactor: 重构 ServerQQ 方法

--- dice/platform_adapter_qq_helper.go
+++ dice/platform_adapter_qq_helper.go
@@ -4,193 +4,209 @@ import "time"
 
 func ServeQQ(d *Dice, ep *EndPointInfo) {
 	defer CrashLog()
-	// 换成 gocq 是不是更好 and 函数名都叫 ServeQQ 了……
-	if ep.Platform == "QQ" && (ep.ProtocolType == "onebot" || ep.ProtocolType == "") {
-		conn := ep.Adapter.(*PlatformAdapterGocq)
-
-		if !conn.DiceServing {
-			conn.DiceServing = true
-		} else {
-			return
-		}
-
-		ep.Enable = true
-		ep.State = 2 // 连接中
-		d.LastUpdatedTime = time.Now().Unix()
-		d.Save(false)
-
-		checkQuit := func() bool {
-			if conn.GoCqhttpState == StateCodeInLoginDeviceLock {
-				d.Logger.Infof("检测到设备锁流程，暂时不再连接")
-				ep.State = 0
-				d.LastUpdatedTime = time.Now().Unix()
-				d.Save(false)
-				return true
-			}
-			if !conn.DiceServing {
-				// 退出连接
-				d.Logger.Infof("检测到连接关闭，不再进行此onebot服务的重连: <%s>(%s)", ep.Nickname, ep.UserID)
-				return true
-			}
-			return false
-		}
-
-		waitTimes := 0
-		for {
-			if checkQuit() {
-				break
-			}
-
-			// 骰子开始连接
-			d.Logger.Infof("开始连接 onebot 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
-			ret := ep.Adapter.Serve()
-
-			if ret == 0 {
-				break
-			}
-
-			if checkQuit() {
-				break
-			}
-
-			waitTimes++
-			if waitTimes > 5 {
-				d.Logger.Infof("onebot 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
-				conn.DiceServing = false
-				break
-			}
-
-			time.Sleep(15 * time.Second)
-		}
+	if ep.Platform != "QQ" {
+		return
 	}
-	// TODO 重复了，写个函数
-	if ep.Platform == "QQ" && ep.ProtocolType == "walle-q" {
+
+	switch ep.ProtocolType {
+	case "walle-q":
 		conn := ep.Adapter.(*PlatformAdapterWalleQ)
+		serverWalleQ(d, ep, conn)
 
-		if !conn.DiceServing {
-			conn.DiceServing = true
-		} else {
-			return
-		}
-
-		ep.Enable = true
-		ep.State = 2 // 连接中
-		d.LastUpdatedTime = time.Now().Unix()
-		d.Save(false)
-
-		checkQuit := func() bool {
-			if conn.WalleQState == StateCodeInLoginDeviceLock {
-				d.Logger.Infof("检测到设备锁流程，暂时不再连接")
-				ep.State = 0
-				d.LastUpdatedTime = time.Now().Unix()
-				d.Save(false)
-				return true
-			} // 暂时去掉设备锁检查
-			if !conn.DiceServing {
-				// 退出连接
-				d.Logger.Infof("检测到连接关闭，不再进行此onebot服务的重连: <%s>(%s)", ep.Nickname, ep.UserID)
-				return true
-			}
-			return false
-		}
-
-		waitTimes := 0
-		for {
-			if checkQuit() {
-				break
-			}
-
-			// 骰子开始连接
-			d.Logger.Infof("开始连接 onebot 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
-			ret := ep.Adapter.Serve()
-
-			if ret == 0 {
-				break
-			}
-
-			if checkQuit() {
-				break
-			}
-
-			waitTimes++
-			if waitTimes > 5 {
-				d.Logger.Infof("onebot 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
-				conn.DiceServing = false
-				break
-			}
-
-			time.Sleep(15 * time.Second)
-		}
-	}
-
-	if ep.Platform == "QQ" && ep.ProtocolType == "red" {
+	case "red":
 		conn := ep.Adapter.(*PlatformAdapterRed)
+		serverRed(d, ep, conn)
 
-		if !conn.DiceServing {
-			conn.DiceServing = true
-		} else {
-			return
-		}
-
-		ep.Enable = true
-		ep.State = 2 // 连接中
-		d.LastUpdatedTime = time.Now().Unix()
-		d.Save(false)
-		waitTimes := 0
-
-		for {
-			// 骰子开始连接
-			d.Logger.Infof("开始连接 red 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
-			ret := ep.Adapter.Serve()
-
-			if ret == 0 {
-				break
-			}
-
-			waitTimes += 1
-			if waitTimes > 5 {
-				d.Logger.Infof("red 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
-				conn.DiceServing = false
-				break
-			}
-
-			time.Sleep(15 * time.Second)
-		}
-	}
-
-	if ep.Platform == "QQ" && ep.ProtocolType == "satori" {
+	case "satori":
 		conn := ep.Adapter.(*PlatformAdapterSatori)
-		if !conn.DiceServing {
-			conn.DiceServing = true
-		} else {
-			return
-		}
-		ep.Enable = true
-		ep.State = 2 // 连接中
-		d.LastUpdatedTime = time.Now().Unix()
-		d.Save(false)
-		waitTimes := 0
+		serverSatori(d, ep, conn)
 
-		for {
-			if ep.State != 2 {
-				break
-			}
-			// 骰子开始连接
-			d.Logger.Infof("开始连接 satori 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
-			ret := ep.Adapter.Serve()
-
-			if ret == 0 {
-				break
-			}
-
-			waitTimes += 1
-			if waitTimes > 5 {
-				d.Logger.Infof("satori 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
-				conn.DiceServing = false
-				break
-			}
-
-			time.Sleep(15 * time.Second)
-		}
+	case "onebot":
+		fallthrough
+	default: // onebot 作为默认情况
+		conn := ep.Adapter.(*PlatformAdapterGocq)
+		serverGocq(d, ep, conn)
 	}
+}
+
+func serverGocq(d *Dice, ep *EndPointInfo, conn *PlatformAdapterGocq) bool {
+	if conn.DiceServing {
+		return true
+	}
+	conn.DiceServing = true
+
+	ep.Enable = true
+	ep.State = 2 // 连接中
+	d.LastUpdatedTime = time.Now().Unix()
+	d.Save(false)
+
+	checkQuit := func() bool {
+		if conn.GoCqhttpState == StateCodeInLoginDeviceLock {
+			d.Logger.Infof("检测到设备锁流程，暂时不再连接")
+			ep.State = 0
+			d.LastUpdatedTime = time.Now().Unix()
+			d.Save(false)
+			return true
+		}
+		if !conn.DiceServing {
+			// 退出连接
+			d.Logger.Infof("检测到连接关闭，不再进行此onebot服务的重连: <%s>(%s)", ep.Nickname, ep.UserID)
+			return true
+		}
+		return false
+	}
+
+	waitTimes := 0
+	for {
+		if checkQuit() {
+			break
+		}
+
+		// 骰子开始连接
+		d.Logger.Infof("开始连接 onebot 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
+		ret := ep.Adapter.Serve()
+
+		if ret == 0 {
+			break
+		}
+
+		if checkQuit() {
+			break
+		}
+
+		waitTimes++
+		if waitTimes > 5 {
+			d.Logger.Infof("onebot 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
+			conn.DiceServing = false
+			break
+		}
+
+		time.Sleep(15 * time.Second)
+	}
+	return false
+}
+
+func serverWalleQ(d *Dice, ep *EndPointInfo, conn *PlatformAdapterWalleQ) bool {
+	if conn.DiceServing {
+		return true
+	}
+	conn.DiceServing = true
+	ep.Enable = true
+	ep.State = 2 // 连接中
+	d.LastUpdatedTime = time.Now().Unix()
+	d.Save(false)
+
+	checkQuit := func() bool {
+		if conn.WalleQState == StateCodeInLoginDeviceLock {
+			d.Logger.Infof("检测到设备锁流程，暂时不再连接")
+			ep.State = 0
+			d.LastUpdatedTime = time.Now().Unix()
+			d.Save(false)
+			return true
+		} // 暂时去掉设备锁检查
+		if !conn.DiceServing {
+			// 退出连接
+			d.Logger.Infof("检测到连接关闭，不再进行此onebot服务的重连: <%s>(%s)", ep.Nickname, ep.UserID)
+			return true
+		}
+		return false
+	}
+
+	waitTimes := 0
+	for {
+		if checkQuit() {
+			break
+		}
+
+		// 骰子开始连接
+		d.Logger.Infof("开始连接 onebot 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
+		ret := ep.Adapter.Serve()
+
+		if ret == 0 {
+			break
+		}
+
+		if checkQuit() {
+			break
+		}
+
+		waitTimes++
+		if waitTimes > 5 {
+			d.Logger.Infof("onebot 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
+			conn.DiceServing = false
+			break
+		}
+
+		time.Sleep(15 * time.Second)
+	}
+	return false
+}
+
+func serverRed(d *Dice, ep *EndPointInfo, conn *PlatformAdapterRed) bool {
+	if conn.DiceServing {
+		return true
+	}
+	conn.DiceServing = true
+
+	ep.Enable = true
+	ep.State = 2 // 连接中
+	d.LastUpdatedTime = time.Now().Unix()
+	d.Save(false)
+	waitTimes := 0
+
+	for {
+		// 骰子开始连接
+		d.Logger.Infof("开始连接 red 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
+		ret := ep.Adapter.Serve()
+
+		if ret == 0 {
+			break
+		}
+
+		waitTimes += 1
+		if waitTimes > 5 {
+			d.Logger.Infof("red 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
+			conn.DiceServing = false
+			break
+		}
+
+		time.Sleep(15 * time.Second)
+	}
+	return false
+}
+
+func serverSatori(d *Dice, ep *EndPointInfo, conn *PlatformAdapterSatori) bool {
+	if conn.DiceServing {
+		return true
+	}
+	conn.DiceServing = true
+
+	ep.Enable = true
+	ep.State = 2 // 连接中
+	d.LastUpdatedTime = time.Now().Unix()
+	d.Save(false)
+	waitTimes := 0
+
+	for {
+		if ep.State != 2 {
+			break
+		}
+		// 骰子开始连接
+		d.Logger.Infof("开始连接 satori 服务，帐号 <%s>(%s)，重试计数[%d/%d]", ep.Nickname, ep.UserID, waitTimes, 5)
+		ret := ep.Adapter.Serve()
+
+		if ret == 0 {
+			break
+		}
+
+		waitTimes += 1
+		if waitTimes > 5 {
+			d.Logger.Infof("satori 连接重试次数过多，先行中断: <%s>(%s)", ep.Nickname, ep.UserID)
+			conn.DiceServing = false
+			break
+		}
+
+		time.Sleep(15 * time.Second)
+	}
+	return false
 }
