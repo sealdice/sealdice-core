39b48a4eb84e08758ee838f9c18d921efee9244e
JustAnotherID <just-another-id@outlook.com>
2024-03-11 23:20:22 +0800

    chore: 版本号时间改为完整 yyyyMMdd

--- dice/dice.go
+++ dice/dice.go
@@ -33,9 +33,9 @@ var (
 	APPNAME = "SealDice"
 
 	// VERSION 版本号，按固定格式，action 在构建时可能会自动注入部分信息
-	// 正式：主版本号+yyMMdd，如 1.4.5+230308
-	// dev：主版本号-dev+yyMMdd.7位hash，如 1.4.5-dev+230308.1a2b3c4
-	// rc：主版本号-rc.序号+yyMMdd.7位hash如 1.4.5-rc.0+230308.1a2b3c4，1.4.5-rc.1+230309.2a3b4c4，……
+	// 正式：主版本号+yyyyMMdd，如 1.4.5+20240308
+	// dev：主版本号-dev+yyyyMMdd.7位hash，如 1.4.5-dev+20240308.1a2b3c4
+	// rc：主版本号-rc.序号+yyyyMMdd.7位hash如 1.4.5-rc.0+20240308.1a2b3c4，1.4.5-rc.1+20240309.2a3b4c4，……
 	VERSION = VERSION_MAIN + VERSION_PRERELEASE + VERSION_BUILD_METADATA
 
 	// VERSION_MAIN 主版本号
