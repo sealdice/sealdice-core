b62e6898683ab3db447cd9742daf3236894f92ae
chire <164668043+yichere@users.noreply.github.com>
2024-05-03 20:25:24 +0800

    feat(js):new api： FileOverwrite，FileAppend

--- dice/dice_jsvm.go
+++ dice/dice_jsvm.go
@@ -461,7 +461,10 @@ func (d *Dice) JsInit() {
 
 		_ = seal.Set("setPlayerGroupCard", SetPlayerGroupCardByTemplate)
 		_ = seal.Set("base64ToImage", Base64ToImageFunc(d.Logger))
+		_ = seal.Set("log", Log(d.Logger))
 		_ = seal.Set("errorlog", ErrorLog(d.Logger))
+		_ = seal.Set("filewrite", FileWrite(d.Logger))
+		_ = seal.Set("fileread", FileRead(d.Logger))
 
 		// Note: Szzrain 暴露dice对象给js会导致js可以调用dice的所有Export的方法
 		// 这是不安全的, 所有需要用到dice实例的函数都可以以传入ctx作为替代
