da00651ab272e4f8f7afaf148aa3ea81f9b11672
JustAnotherID <just-another-id@outlook.com>
2024-06-07 20:14:19 +0800

    feat(Store): 完善扩展商店的 api

--- api/store.go
+++ api/store.go
@@ -5,19 +5,24 @@ import (
 )
 
 type storeElem struct {
-	Source       string            `json:"source"` // official
-	Type         string            `json:"type"`   // plugin | deck | reply
-	Ext          string            `json:"ext"`    // .js | .json
-	Key          string            `json:"key"`    // [<author>]<name>(<version>), e.g. [JustAnotherID]Demo(1.0.0)
-	Namespace    string            `json:"namespace"`
+	Namespace string `json:"namespace"`
+	ID        string `json:"id"`
+	Version   string `json:"version"`
+	Key       string `json:"key"` // @<namespace>/<id>@<version>, e.g. @seal/example@1.0.0
+	Installed bool   `json:"installed"`
+
+	Source string `json:"source"` // official
+	Type   string `json:"type"`   // plugin | deck | reply
+	Ext    string `json:"ext"`    // .js | .json |...
+
 	Name         string            `json:"name"`
 	Authors      []string          `json:"authors"`
-	Version      string            `json:"version"`
-	License      string            `json:"license"`
 	Desc         string            `json:"desc"`
+	License      string            `json:"license"`
 	ReleaseTime  uint64            `json:"releaseTime"`
 	UpdateTime   uint64            `json:"updateTime"`
 	Tags         []string          `json:"tags"`
+	Rate         float64           `json:"rate"`
 	Extra        map[string]string `json:"extra"`
 	DownloadNum  uint64            `json:"downloadNum"`
 	DownloadUrl  string            `json:"downloadUrl"`
@@ -27,42 +32,42 @@ type storeElem struct {
 	Dependencies map[string]string `json:"dependencies"`
 }
 
+var demos = []storeElem{
+	{
+		Source:    "official",
+		Type:      "plugin",
+		Ext:       ".js",
+		Key:       "js-1",
+		Namespace: "JustAnotherID",
+		Name:      "野兽插件",
+		Authors:   []string{"JustAnotherID"},
+		Version:   "1.0.0",
+		License:   "MIT",
+		Desc:      "测试用占位插件",
+		Tags: []string{
+			"下北泽",
+			"会员制插件",
+			"逸一时误一世",
+			"逸久逸久罢已龄",
+		},
+		Rate:        5,
+		ReleaseTime: 1716867914,
+		UpdateTime:  1716867914,
+		Extra:       nil,
+		DownloadNum: 114514,
+		DownloadUrl: "",
+	},
+}
+
+func storeRecommend(c echo.Context) error {
+	return Success(&c, Response{
+		"data": demos,
+	})
+}
+
 func storeGetPage(c echo.Context) error {
 	return Success(&c, Response{
-		"data": []storeElem{
-			{
-				Source:      "official",
-				Type:        "plugin",
-				Ext:         ".js",
-				Key:         "js-1",
-				Name:        "测试插件1",
-				Authors:     []string{"JustAnotherID"},
-				Version:     "1.0.0",
-				License:     "MIT",
-				Desc:        "测试插件1",
-				ReleaseTime: 1714318458,
-				UpdateTime:  1714318458,
-				Extra:       nil,
-				DownloadNum: 114514,
-				DownloadUrl: "",
-			},
-			{
-				Source:      "official",
-				Type:        "plugin",
-				Ext:         ".js",
-				Key:         "js-2",
-				Name:        "测试插件2",
-				Authors:     []string{"JustAnotherID"},
-				Version:     "1.0.0",
-				License:     "MIT",
-				Desc:        "测试插件2-1\n测试插件2-2\n测试插件2-3",
-				ReleaseTime: 1714318458,
-				UpdateTime:  1714318458,
-				Extra:       nil,
-				DownloadNum: 1919810,
-				DownloadUrl: "",
-			},
-		},
+		"data":     demos,
 		"total":    2,
 		"pageNum":  1,
 		"pageSize": 2,
@@ -70,5 +75,9 @@ func storeGetPage(c echo.Context) error {
 }
 
 func storeDownload(c echo.Context) error {
+	return Error(&c, "not implemented", Response{})
+}
+
+func storeRating(c echo.Context) error {
 	return Error(&c, "not implemented", Response{})
 }
