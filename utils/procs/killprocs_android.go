1b53235faf26d7bf095273cfa069df0cdd0a657e
Fripine <84258717+Fripine@users.noreply.github.com>
2024-07-10 13:05:05 +0800

    fix: 安卓平台无法正常杀死内置Lagrange子进程 (#870)
    (cherry picked from commit 8fe0338a8b43cb8b7d2581c2cb56f406a2181e32)

---
+++ utils/procs/killprocs_android.go
@@ -0,0 +1,17 @@
+//go:build android
+// +build android
+
+package procs
+
+import (
+	"syscall"
+)
+
+func (p *Process) KillProcess() error {
+	cmd := p.Cmd
+	return syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+}
+func (p *Process) Setpgid() {
+	cmd := p.Cmd
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+}
