16b97f3d8345519a7bfd239cff9d7a9c23d65128
fy <fy0748@gmail.com>
2024-07-20 21:13:28 +0800

    fix: ImagePreviewWidget

--- story-painter/src/components/CodeMirror.vue
+++ story-painter/src/components/CodeMirror.vue
@@ -133,7 +133,7 @@ function imagePreviews(view: EditorView): DecorationSet {
           if (m) {
             const url = `https://gchat.qpic.cn/gchatpic_new/0/0-0-${m[1]}${m[2]}${m[3]}${m[4]}${m[5]}/0?term=2,subType=1`
             let deco = Decoration.widget({
-              widget: new CheckboxWidget(url),
+              widget: new ImagePreviewWidget(url),
               side: 0
             })
             widgets.push(deco.range(to))
