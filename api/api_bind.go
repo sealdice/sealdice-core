544e22bdcb430e30fcca55971f3861b21f946104
Pinenutn <1101839859@qq.com>
2024-10-27 15:29:33 +0800

    Revert "Revert "feat: 增加多个子模块 vm engine 开关 (#1086)""
    This reverts commit 6f6bd84b3b1c89bcbaa5bd0fa7187c63b12ea8a2.

--- api/api_bind.go
+++ api/api_bind.go
@@ -638,8 +638,7 @@ func Bind(e *echo.Echo, _myDice *dice.DiceManager) {
 	e.GET(prefix+"/dice/cmdList", DiceAllCommand)
 	e.POST(prefix+"/dice/upload_to_upgrade", DiceNewVersionUpload)
 
-	e.POST(prefix+"/dice/config/vm-version-for-reply-set", vmVersionForReplySet)
-	e.POST(prefix+"/dice/config/vm-version-for-deck-set", vmVersionForDeckSet)
+	e.POST(prefix+"/dice/config/vm-version-set", vmVersionSet)
 
 	e.POST(prefix+"/signin", doSignIn)
 	e.GET(prefix+"/signin/salt", doSignInGetSalt)
