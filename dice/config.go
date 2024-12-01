aa07df41418ae1dca4429e20325473c5323d1d87
Fripine <Fripine@hotmail.com>
2024-12-01 13:40:12 +0800

    fix: 补齐其余配置项默认值读取

--- dice/config.go
+++ dice/config.go
@@ -2316,8 +2316,10 @@ func (d *Dice) loads() {
 	} else {
 		d.Logger.Info("serve.yaml not found")
 		// 这里是没有加载到配置文件，所以写默认设置项
-		d.DiceMasters = config.DiceMasters
+		d.DeckList = config.DeckList
 		d.CommandPrefix = config.CommandPrefix
+		d.DiceMasters = config.DiceMasters
+		d.Config = config
 	}
 
 	_ = model.BanItemList(d.DBData, func(id string, banUpdatedAt int64, data []byte) {
