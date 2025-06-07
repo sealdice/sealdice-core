6846ab19a588bfef46b7ebf769352913d838449d
Pinenutn <1101839859@qq.com>
2025-06-08 00:39:10 +0800

    refactor: 重构升级模块，并部分调整main的排序

---
+++ utils/upgrader/upgrade.go
@@ -0,0 +1,28 @@
+package upgrade
+
+import (
+	"time"
+
+	"sealdice-core/utils/dboperator/engine"
+)
+
+type Upgrade struct {
+	ID          string
+	Description string
+	// TODO: 有更好的想法吗，需要啥就从这里传是不是太抽象了
+	Apply func(logf func(string), operator engine.DatabaseOperator) error
+}
+
+type UpgradeRecord struct {
+	ID        string    `json:"id"`
+	Timestamp time.Time `json:"timestamp"`
+	Success   bool      `json:"success"`
+	Message   string    `json:"message"`
+	Logs      []string  `json:"logs"`
+}
+
+type Store interface {
+	IsApplied(id string) (bool, error)
+	SaveRecord(record UpgradeRecord) error
+	LoadRecords() ([]UpgradeRecord, error)
+}
