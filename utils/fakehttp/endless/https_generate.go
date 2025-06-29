8e137c1d6073d4447569fe1919c1fcfcc5836132
Pinenutn <1101839859@qq.com>
2025-06-29 18:54:04 +0800

    fix: 文件特化到data目录

--- utils/fakehttp/endless/https_generate.go
+++ utils/fakehttp/endless/https_generate.go
@@ -11,35 +11,50 @@ import (
 	"math/big"
 	"net"
 	"os"
+	"path/filepath"
 	"time"
 )
 
-// generateSelfSignedCert 会生成一个自签名证书并返回证书和私钥的临时路径
-func generateSelfSignedCert() (certPath, keyPath string, err error) {
-	// 存储证书和私钥
-	certFile, err := os.Create("cert.crt")
+// generateSelfSignedCert 会生成一个自签名证书
+func generateSelfSignedCert(certPath, keyPath string) (err error) {
+	// 先检查certPath和keyPath的文件夹是否存在，若不存在创建文件夹
+	certDir := filepath.Dir(certPath)
+	keyDir := filepath.Dir(keyPath)
+	if _, err := os.Stat(certDir); os.IsNotExist(err) {
+		err = os.MkdirAll(certDir, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create cert directory: %v", err)
+		}
+	}
+	if _, err := os.Stat(keyDir); os.IsNotExist(err) {
+		err = os.MkdirAll(keyDir, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create key directory: %v", err)
+		}
+	}
+	// 创建文件，并开始生成自签的公钥和私钥
+	certFile, err := os.Create(certPath)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to create temp certificate file: %v", err)
+		return fmt.Errorf("failed to create cert file: %v", err)
 	}
 	defer certFile.Close()
 
-	keyFile, err := os.Create("cert.key")
+	keyFile, err := os.Create(keyPath)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to create temp key file: %v", err)
+		return fmt.Errorf("failed to create key file: %v", err)
 	}
 	defer keyFile.Close()
-
 	// 生成 ECDSA 私钥
 	priv, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to generate private key: %v", err)
+		return fmt.Errorf("failed to generate private key: %v", err)
 	}
 
 	// 生成随机序列号
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to generate serial number: %v", err)
+		return fmt.Errorf("failed to generate serial number: %v", err)
 	}
 
 	// 获取所有网络接口的IP地址
@@ -68,53 +83,60 @@ func generateSelfSignedCert() (certPath, keyPath string, err error) {
 
 	// 创建证书模板
 	template := x509.Certificate{
+		IsCA:         false,
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			Organization: []string{"good.sealdice.com"},
+			Country:            []string{"CN"},
+			Organization:       []string{"SealDice"},
+			OrganizationalUnit: []string{"SealDice"},
+			CommonName:         "self.sealdice.com",
 		},
 		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(365 * 24 * time.Hour), // 证书有效期一年
+		NotAfter:              time.Now().Add(3650 * 24 * time.Hour), // 证书有效期十年
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		BasicConstraintsValid: true,
 		// 添加SAN (Subject Alternative Names) 包含所有网络接口IP
 		IPAddresses: ipAddresses,
 		DNSNames: []string{
-			"weizaima.com",
-			"*.sealdice.com",
+			"*.self.weizaima.com",
+			"*.self.sealdice.com",
 		},
 	}
 
 	// 生成自签名证书
 	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to create certificate: %v", err)
+		return fmt.Errorf("failed to create certificate: %v", err)
 	}
 
-	// 将证书写入临时文件
+	// 将证书写入文件
 	err = pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
 	if err != nil {
-		return "", "", fmt.Errorf("failed to encode certificate to PEM: %v", err)
+		return fmt.Errorf("failed to encode certificate to PEM: %v", err)
 	}
 
-	// 将私钥写入临时文件
+	// 将私钥写入文件
 	privBytes, err := x509.MarshalECPrivateKey(priv)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to marshal private key: %v", err)
+		return fmt.Errorf("failed to marshal private key: %v", err)
 	}
 	err = pem.Encode(keyFile, &pem.Block{Type: "EC PRIVATE KEY", Bytes: privBytes})
 	if err != nil {
-		return "", "", fmt.Errorf("failed to encode private key to PEM: %v", err)
+		return fmt.Errorf("failed to encode private key to PEM: %v", err)
 	}
 
-	return certFile.Name(), keyFile.Name(), nil
+	return nil
 }
 
-// checkOrGenerateCert 检查证书和私钥文件是否存在，如果不存在则生成临时的
+// checkOrGenerateCert 检查证书和私钥文件是否存在，若不存在，生成自签证书
 func CheckOrGenerateCert(certPath, keyPath string) (string, string, error) {
 	// 如果文件不存在，生成临时文件
 	if _, err := os.Stat(certPath); os.IsNotExist(err) {
-		return generateSelfSignedCert()
+		err = generateSelfSignedCert(certPath, keyPath)
+		if err != nil {
+			return "", "", err
+		}
 	}
 	// 如果文件存在，直接返回路径
 	return certPath, keyPath, nil
