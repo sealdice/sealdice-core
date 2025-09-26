976d499a087ba4d69a281ffeff26888b508e8276
bugtower100 <2889352839@qq.com>
2025-09-26 17:40:36 +0800

    draft: 一个接口脚手架，尚没有解决middleware调用dice的问题

---
+++ api/v2/middleware/auth.go
@@ -0,0 +1,9 @@
+package middleware
+
+//func doAuth(c echo.Context) bool {
+//	token := c.Request().Header.Get("token") //nolint:canonicalheader // private header
+//	if token == "" {
+//		token = c.QueryParam("token")
+//	}
+//	return myDice.Parent.AccessTokens.Exists(token)
+//}
--- api/v2/router.go
+++ api/v2/router.go
@@ -1,1 +1,23 @@
 package v2
+
+import (
+	"github.com/danielgtaylor/huma/v2"
+
+	"sealdice-core/api/v2/base"
+	"sealdice-core/dice"
+)
+
+// InitV2Router 初始化v2版本的API路由
+// 使用依赖注入模式，将dice实例传递给各个模块
+func InitV2Router(api huma.API, dice *dice.Dice) {
+	// 注册base模块
+	baseService := base.NewBaseService(dice)
+	baseService.RegisterRoutes(api)
+
+	// TODO: 后续可以在这里添加其他模块
+	// configService := config.NewConfigService(dice)
+	// configService.RegisterRoutes(api)
+
+	// groupService := group.NewGroupService(dice)
+	// groupService.RegisterRoutes(api)
+}
