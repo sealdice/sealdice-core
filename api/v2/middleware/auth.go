45beb1fc0db5ce4c888e90a33ae8be315a722581
bugtower100 <2889352839@qq.com>
2025-09-30 13:46:41 +0800

    闭包。

--- api/v2/middleware/auth.go
+++ api/v2/middleware/auth.go
@@ -1,9 +1,20 @@
 package middleware
 
-//func doAuth(c echo.Context) bool {
-//	token := c.Request().Header.Get("token") //nolint:canonicalheader // private header
-//	if token == "" {
-//		token = c.QueryParam("token")
-//	}
-//	return myDice.Parent.AccessTokens.Exists(token)
-//}
+import (
+	"github.com/danielgtaylor/huma/v2"
+
+	"sealdice-core/dice"
+)
+
+func AuthMiddleware(d *dice.Dice) func(ctx huma.Context, next func(huma.Context)) {
+	return func(ctx huma.Context, next func(huma.Context)) {
+		token := ctx.Header("token")
+		if token == "" {
+			token = ctx.Query("token")
+		}
+		if d.Parent.AccessTokens.Exists(token) {
+			next(ctx)
+		}
+		// 失败登录 400 错误
+	}
+}
