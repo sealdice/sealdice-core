01b59c5ac4d55f7b5a39b39441149e6785616dda
Szzrain <szzrain@szzrain.com>
2025-11-28 15:06:17 -0500

    fix: 修复 coc7 扩展会在开启时修改角色卡模板的问题

--- dice/ext_coc7.go
+++ dice/ext_coc7.go
@@ -1370,11 +1370,6 @@ func RegisterBuiltinExtCoc7(self *Dice) {
 
 		},
 		OnCommandReceived: func(ctx *MsgContext, msg *Message, cmdArgs *CmdArgs) {
-			if ctx != nil && ctx.Dice != nil {
-				if tmpl, ok := ctx.Dice.GameSystemMap.Load("coc7"); ok && tmpl != nil {
-					ctx.SystemTemplate = tmpl
-				}
-			}
 		},
 		GetDescText: GetExtensionDesc,
 		CmdMap: CmdMapCls{
