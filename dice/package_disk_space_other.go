e6ec9a98762293ee50368aba8423cc1ff933c9e0
fy <fy0748@gmail.com>
2026-08-02 01:02:29 +0800

    perf(store): 降低扩展包安装的内存占用 (#1770)

---
+++ dice/package_disk_space_other.go
@@ -0,0 +1,9 @@
+//go:build !windows && !aix && !darwin && !dragonfly && !freebsd && !linux
+
+package dice
+
+import "errors"
+
+func platformPackageDiskSpace(string) (packageDiskSpace, error) {
+	return packageDiskSpace{}, errors.New("当前平台不支持磁盘空间探测")
+}
