9f39de383ddfbc44fbe0c934f42bd491e0420ddc
暮星 <1009592348@qq.com>
2026-08-15 22:33:06 +0800

    fix(build): restore Linux fsync and symlink helpers
    The Linux-specific rename file was missing syncRestoreDirectoryPath and
    isLinkedRestorePath after the rebase, breaking dice compilation on Linux CI.

--- dice/atomic_replace_linux.go
+++ dice/atomic_replace_linux.go
@@ -3,6 +3,7 @@
 package dice
 
 import (
+	"errors"
 	"os"
 
 	"golang.org/x/sys/unix"
@@ -14,4 +15,16 @@ func replaceFileAtomic(source, target string) error {
 
 func renameRestorePath(source, target string) error {
 	return unix.Renameat2(unix.AT_FDCWD, source, unix.AT_FDCWD, target, unix.RENAME_NOREPLACE)
+}
+
+func syncRestoreDirectoryPath(directory string) error {
+	file, err := os.Open(directory)
+	if err != nil {
+		return err
+	}
+	return errors.Join(file.Sync(), file.Close())
+}
+
+func isLinkedRestorePath(info os.FileInfo) bool {
+	return info.Mode()&os.ModeSymlink != 0
 }
